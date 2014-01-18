262fff4485a326e439650665d74e15ebcdce5430
bright.chen.1@gmail.com <bright.chen.1@gmail.com@ea20bc83-2afb-b267-cf91-197c6eaea3fc>
2014-01-18 10:44:34 +0000

    other project 01-18

--- user/src/main/java/my/bc/user/jpa/UserDaoJpa.java
+++ user/src/main/java/my/bc/user/jpa/UserDaoJpa.java
@@ -1,9 +1,12 @@
 package my.bc.user.jpa;
 
+import org.springframework.stereotype.Component;
+
 import my.bc.persistent.jpa.GenericDaoJpa;
 import my.bc.user.dao.UserDao;
 import my.bc.user.model.User;
 
+@Component
 public class UserDaoJpa extends GenericDaoJpa< User > implements UserDao
 {
 
--- web/src/main/java/my/bc/web/listener/PopulatePropertiesListener.java
+++ web/src/main/java/my/bc/web/listener/PopulatePropertiesListener.java
@@ -1,32 +1,54 @@
 package my.bc.web.listener;
 
-import javax.annotation.Resource;
+import java.io.File;
+import java.net.URL;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
-import my.bc.configure.PropertiesManager;
+import my.bc.configure.DefaultPropertiesManager;
 
 /**
- * this class populates properties in order to fulfill the spring placeholder configuration
+ * this class populates properties in order to fulfill the spring placeholder
+ * configuration So, this listener should be called before
+ * ContextLoaderListener, which means this listener should NOT take advantage of
+ * spring bean DI.
  * 
  * @author Bright Chen
- *
+ * 
  */
 public class PopulatePropertiesListener implements ServletContextListener
 {
-  @Resource( name="defaultPropertiesManager" )
-  private PropertiesManager propertiesManager;
-  
   @Override
-  public void contextDestroyed(ServletContextEvent arg0)
+  public void contextDestroyed(ServletContextEvent contextEvent)
   {
   }
 
+  /**
+   * in fact, we only need to populate the properties which stored in the files out of the application.
+   * the Placeholder can be configured use internal property files as default 
+   */
   @Override
-  public void contextInitialized(ServletContextEvent arg0)
+  public void contextInitialized(ServletContextEvent contextEvent)
   {
-    propertiesManager.populateProperties();
+//    try
+//    {
+//      ServletContext context = contextEvent.getServletContext();
+//      URL url = context.getResource("my/bc/persistent/persistent.properties");
+//      if( url != null )
+//        System.out.println( url.toString() );
+//
+//      String realPath = context.getRealPath("/");
+//      realPath += "WEB-INF\\lib\\my\\bc\\persistent\\persistent.properties";
+//      File file = new File(realPath);
+//      System.out.println(file.exists());
+//      DefaultPropertiesManager.instance.populateProperties();
+//    }
+//    catch (Exception e)
+//    {
+//      e.printStackTrace();
+//    }
   }
-
 
 }
