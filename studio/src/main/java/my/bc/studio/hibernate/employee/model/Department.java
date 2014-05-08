565653aa6f529db66488be4e59c271e33ce0cb14
bright.chen.1@gmail.com <bright.chen.1@gmail.com@ea20bc83-2afb-b267-cf91-197c6eaea3fc>
2014-05-08 00:26:07 +0000

    add hibernate mapping test

---
+++ studio/src/main/java/my/bc/studio/hibernate/employee/model/Department.java
@@ -0,0 +1,40 @@
+package my.bc.studio.hibernate.employee.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "department")
+public class Department
+{
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Integer id;
+
+  private String name;
+
+  public Integer getId()
+  {
+    return id;
+  }
+
+  public void setId( Integer id )
+  {
+    this.id = id;
+  }
+
+  public String getName()
+  {
+    return name;
+  }
+
+  public void setName( String name )
+  {
+    this.name = name;
+  }
+  
+  
+}
---
+++ studio/src/main/java/my/bc/studio/hibernate/employee/model/Employee.java
@@ -0,0 +1,88 @@
+package my.bc.studio.hibernate.employee.model;
+
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "employee")
+public class Employee
+{
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Integer id;
+  private String  firstName;
+  private String  lastName;
+  private Integer salary;
+  
+  @ManyToMany(cascade = {CascadeType.ALL})
+  @JoinTable(name="employee_role", 
+              joinColumns={@JoinColumn(name="employee_id", referencedColumnName="id")}, 
+              inverseJoinColumns={@JoinColumn(name="role_id", referencedColumnName="id")})
+  private Set<Role> employeeRoles;
+
+  @ManyToOne
+  @JoinColumn(name="deparment_id", nullable=true, updatable=true)
+  private Department department;
+  
+  public Employee()
+  {
+  }
+
+  public Employee( String fname, String lname, int salary )
+  {
+    this.firstName = fname;
+    this.lastName = lname;
+    this.salary = salary;
+  }
+
+  public int getId()
+  {
+    return id;
+  }
+
+  public void setId( int id )
+  {
+    this.id = id;
+  }
+
+  public String getFirstName()
+  {
+    return firstName;
+  }
+
+  public void setFirstName( String first_name )
+  {
+    this.firstName = first_name;
+  }
+
+  public String getLastName()
+  {
+    return lastName;
+  }
+
+  public void setLastName( String last_name )
+  {
+    this.lastName = last_name;
+  }
+
+  public int getSalary()
+  {
+    return salary;
+  }
+
+  public void setSalary( int salary )
+  {
+    this.salary = salary;
+  }
+}
