0a358c6223c169d3b2303ed2fb3b7c668bcffce8
bright.chen.1@gmail.com <bright.chen.1@gmail.com@ea20bc83-2afb-b267-cf91-197c6eaea3fc>
2014-05-10 01:23:50 +0000

    add test case

--- trunk/studio/src/main/java/my/bc/studio/smartdbtool/service/UserService.java
+++ trunk/studio/src/main/java/my/bc/studio/smartdbtool/service/UserService.java
@@ -2,38 +2,66 @@ package my.bc.studio.smartdbtool.service;
 
 import java.util.List;
 
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.stereotype.Service;
+
+import my.bc.smartdbtool.criteria.IQueryCriteria;
+import my.bc.smartdbtool.criteria.IQueryCriteriaBuilder;
+import my.bc.smartdbtool.criteria.QueryCriteriaBuilder;
+import my.bc.smartdbtool.criteria.SmartQueryBuilder;
+import my.bc.studio.model.Account;
+import my.bc.studio.model.Role;
 import my.bc.studio.model.User;
 import my.bc.studio.smartdbtool.searchcrieria.UserSearchCriteria;
 
+@Service
 public class UserService implements IUserService
 {
+  private Session hibernateSession;
+  
+  public void setHibernateSession( Session hibernateSession )
+  {
+    this.hibernateSession = hibernateSession;
+  }
+  
+  protected <T> T findByName( Class<T> persistentClass, String name )
+  {
+    Criteria criteria = hibernateSession.createCriteria( persistentClass ).add( Restrictions.eq( "name", name ) );
+    List<T> entities = criteria.list();
+    return entities.isEmpty() ? null : entities.get( 0 );
+    
+  }
+  
   @Override
   public User findUserByName( String name )
   {
-    return findEntityByName( User.class, name, false );
+    return findByName( User.class, name );
   }
 
   @Override
   public Account findAccountByName( String name )
   {
-    return findEntityByName( Account.class, name, false );
+    return findByName( Account.class, name );
   }
 
   @Override
-  public Role findRoleByName( String roleName )
+  public Role findRoleByName( String name )
   {
-    return findEntityByName( Role.class, roleName, false );
+    return findByName( Role.class, name );
   }
   
   @Override
+  @SuppressWarnings( "unchecked" )
   public List< User > findUsers( UserSearchCriteria criteria )
   {
     // most of property is equals ignore case
     IQueryCriteria queryCriteria = QueryCriteriaBuilder.defaultInstance().buildCriteria( User.class, criteria, IQueryCriteriaBuilder.QueryRelation.LikeIgnoreCase );
     String query = SmartQueryBuilder.defaultInstance().buildSearchHsql( User.class, queryCriteria );
-    @SuppressWarnings( "unchecked" )
     List<User> users = (List<User>)getEntityManager().createQuery( query  ).getResultList();
-    return ViewUtil.entitiesToReadableViews( users, UserSearchView.class );
+    
+    return users;
   }
 
 
--- trunk/studio/src/main/java/my/bc/studio/smartdbtool/test/SimpleTest1.java
+++ trunk/studio/src/main/java/my/bc/studio/smartdbtool/test/SimpleTest1.java
@@ -1,16 +1,33 @@
 package my.bc.studio.smartdbtool.test;
 
-import my.bc.studio.smartdbtool.searchcrieria.UserSearchCriteria;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
 
+import my.bc.studio.smartdbtool.searchcrieria.UserSearchCriteria;
+import my.bc.studio.smartdbtool.service.IUserService;
+import my.bc.studio.smartdbtool.service.UserService;
+
+@Component
 public class SimpleTest1
 {
+  @Autowired( required = true )
+  private IUserService userService;
+  
   public static void main( String[] argvs )
   {
+    final ApplicationContext context = SpringBeanConfigurator.DEFAULT.getApplicationContext();
+    SimpleTest1 test = context.getBean( SimpleTest1.class );
+
+    test.run();
+  }
+
+  public void run()
+  {
+
     UserSearchCriteria searchCriteria = new UserSearchCriteria();
     searchCriteria.setFirstName( "firstname" );
     searchCriteria.setLastName( "lastname" );
-    IUserService userService = new UserService();
     userService.findUsers( searchCriteria );
   }
-
 }
