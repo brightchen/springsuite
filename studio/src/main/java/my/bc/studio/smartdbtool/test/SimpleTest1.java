445e2876bc8a68d9b51831efce1d873bd27b3542
bright.chen.1@gmail.com <bright.chen.1@gmail.com@ea20bc83-2afb-b267-cf91-197c6eaea3fc>
2014-05-14 01:16:31 +0000

    add junit test

--- studio/src/main/java/my/bc/studio/smartdbtool/test/SimpleTest1.java
+++ studio/src/main/java/my/bc/studio/smartdbtool/test/SimpleTest1.java
@@ -2,6 +2,13 @@ package my.bc.studio.smartdbtool.test;
 
 import java.util.List;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
@@ -11,21 +18,40 @@ import my.bc.studio.smartdbtool.searchcrieria.UserSearchCriteria;
 import my.bc.studio.smartdbtool.service.IUserService;
 import my.bc.studio.smartdbtool.service.UserService;
 
-@Component
+@RunWith(JUnit4.class)
 public class SimpleTest1
 {
+  private static ApplicationContext applicationContext;
+  
   @Autowired( required = true )
   private IUserService userService;
   
-  public static void main( String[] argvs )
+  @BeforeClass
+  public static void classSetUp() 
   {
-    final ApplicationContext context = SpringBeanConfigurator.DEFAULT.getApplicationContext();
-    SimpleTest1 test = context.getBean( SimpleTest1.class );
-
-    test.run();
+    applicationContext = SpringBeanConfigurator.DEFAULT.getApplicationContext();
   }
 
-  public void run()
+  @AfterClass
+  public static void classTearDown() 
+  {
+    SpringBeanConfigurator.DEFAULT.closeApplicationContext();
+  }
+
+  @Before
+  public void prepareData()
+  {
+    
+  }
+  
+  @After
+  public void cleanData()
+  {
+    
+  }
+  
+  @Test
+  public void queryInsideEntity()
   {
     UserSearchCriteria searchCriteria = new UserSearchCriteria();
     searchCriteria.setFirstName( "firstname" );
